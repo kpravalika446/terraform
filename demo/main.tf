b3b3dd2cf095bc4362ecfda62ab69b670a9c8e47
kpravalika446 <80943125+kpravalika446@users.noreply.github.com>
2021-03-19 13:58:39 +0530

    Update main.tf

--- demo/main.tf
+++ demo/main.tf
@@ -5,13 +5,13 @@ features {}
 
 resource "azurerm_resource_group" "example" {
 name = "newrg"
-location = "eastus"
+location = "west us"
 }
 
 resource "azurerm_storage_account" "example" {
-name = "pravalikastorgrs1234"
+name = "pravalikastorgrs12345"
 resource_group_name = azurerm_resource_group.example.name
 location = "eastus"
 account_tier = "Standard"
-account_replication_type = "GRS"
+account_replication_type = "LRS"
 }
